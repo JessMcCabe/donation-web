5250178812ecef29ee695fa6435d48e0127d37c6
Jessica McCabe <mccabejessica88@gmail.com>
2020-02-05 20:20:09 +0000

    Lab04B Models

---
+++ app/models/donation.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Mongoose = require('mongoose');
+const Schema = Mongoose.Schema;
+
+const donationSchema = new Schema({
+    amount: Number,
+    method: String,
+    donor: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+module.exports = Mongoose.model('Donation', donationSchema);
